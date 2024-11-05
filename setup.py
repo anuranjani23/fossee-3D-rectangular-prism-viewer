39b524b9f25873973bf59a15af65ca03c01a0ce3
anuranjani23 <anuranjani6174@gmail.com>
2024-11-05 18:48:14 +0530

    chore: modify PIP project

--- setup.py
+++ setup.py
@@ -1,37 +1,35 @@
 from setuptools import setup, find_packages
 
 setup(
-
-name="prism-viewer",
-
-version="0.1.0",
-
-packages=find_packages(),
-
-install_requires=[
-
-"PyQt5",
-
-"numpy",
-
-"OCC-Core",
-
-],
-
-entry_points={
-
-'console_scripts': [
-
-'prism-viewer=main:main',
-
-],
-
-},
-
-author="anuranjani",
-
-description="A PyQt5 and PythonOCC based 3D rectangular prism viewer application.",
-
-python_requires=">=3.8",
-
+    name="prism_viewer",
+    version="0.1.1",
+    author="Anuranjani",
+    description="A PyQt5 and PythonOCC-based 3D rectangular prism viewer application",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/anuranjani23/fossee-3D-rectangular-prism-viewer",
+    license="MIT",
+    packages=find_packages(where="prism_viewer"),
+    package_dir={"": "prism_viewer"},
+    include_package_data=True,
+    install_requires=[
+        "numpy==1.26.4",
+        "swig",
+        "pythonocc-core==7.8.1",
+        "pyqt==5.15.9",
+        "six==1.16.0",
+        "svgwrite",
+    ],
+    entry_points={
+        "console_scripts": [
+            "prism_viewer=prism_viewer.main:main",
+        ],
+    },
+    python_requires=">=3.11",
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    test_suite="prism_viewer.tests",
 )
